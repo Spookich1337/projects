1947c28a2f6361cca574fffbfc334615321a908e
Spookich <Yagudin-05@mail.ru>
2026-03-29 23:31:17 +0300

    done models for db

--- src/database/DBmodels.py
+++ src/database/DBmodels.py
@@ -1,9 +1,17 @@
-from sqlalchemy import  Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Integer, String, ForeignKey, Table, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import ARRAY
 from typing import List
 
 Base = declarative_base()
+
+user_subscriptions = Table(
+    "user_subscriptions",
+    Base.metadata,
+    Column('subscriber_id', Integer, ForeignKey('users.id'), primary_key=True),
+    Column('subscribed_to_id', Integer, ForeignKey('users.id'), primary_key=True)
+)
 
 class User(Base):
     __tablename__ = "users"
@@ -12,7 +20,20 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(16))
     email: Mapped[str] = mapped_column(String(254))
     password: Mapped[str] = mapped_column(String(32))
-    subscribe: Mapped[List["User.id"]] = mapped_column(int)
+    subscriptions: Mapped[List["User"]] = relationship(
+        "User",
+        secondary=user_subscriptions,
+        primaryjoin=id == user_subscriptions.c.subscriber_id,
+        secondaryjoin=id == user_subscriptions.c.subscribed_to_id,
+        back_populates="subscribers"
+    )
+    subscribers: Mapped[List["User"]] = relationship(
+        "User",
+        secondary=user_subscriptions,
+        primaryjoin=id == user_subscriptions.c.subscribed_to_id,
+        secondaryjoin=id == user_subscriptions.c.subscriber_id,
+        back_populates="subscriptions"
+    )
     posts: Mapped[List["Post"]] = relationship(
         "Post", 
         back_populates="author", 
@@ -25,4 +46,6 @@ class Post(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     author_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
     title: Mapped[str] = mapped_column(String(64))
-    text: Mapped[str] = mapped_column(String(300))
+    text: Mapped[str] = mapped_column(String(300), nullable=True)
+    rating_up: Mapped[list[int]] = mapped_column(ARRAY(Integer, dimensions=1), default=list)
+    rating_down: Mapped[list[int]] = mapped_column(ARRAY(Integer, dimensions=1), default=list)
